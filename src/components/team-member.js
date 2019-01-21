efe600ca64555f6be6a560d5dadda9dac347a130
Junerey Casuga <junerey.casuga@gmail.com>
2019-01-21 20:22:15 +0700

    Updates

--- src/components/team-member.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default ({ member }) => (
-  <div key={member.id} className="col-md-3">
-    <img src={member.image.file.url} className="img-responsive" />
-    <p>{member.name}</p>
-    <small>{member.shortBio.shortBio}</small>
-  </div>
-)
