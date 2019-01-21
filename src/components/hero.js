4b140d10b0e0db37f4de01f11890ef81e921aeee
Junerey Casuga <junerey.casuga@gmail.com>
2019-01-21 21:02:10 +0700

    fix background on hero section

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,11 +3,11 @@ import Img from 'gatsby-image'
 
 export default ({ data }) => (
   <div className="row text-center section section-hero">
-    <div className="container-fluid">
+    <div className="container-fluid hero-text-container">
       <h1 className="hero-text text-center">{data.tagline}</h1>
       <a href="#" role="button" className="btn btn-hero btn-yellow text-center">Ask for price</a>
-      <div className="hero-image-container"></div>
     </div>
+    <div className="hero-image-container"></div>
     {/* <img src="/images/hero_bg.svg" className="hero-image" /> */}
   </div>
 )
